01d373409aa8b5f30f2e022341639f6dd8d6e1dc
Marcelo Oviedo <oviedommarcelo@gmail.com>
2024-01-06 17:28:30 -0300

    Fix some comments

--- src/dao/order.mongodb.dao.js
+++ src/dao/order.mongodb.dao.js
@@ -4,27 +4,27 @@ import OrderModel from '../models/order.model.js'
 
 export default class OrderDao {
 
-    // Create a new user in the database.
+    // Create a new order in the database.
     create = (data) => {
         return OrderModel.create(data)
     }
 
-    // Get all users from the database.
+    // Get all orders from the database.
     getall = () => {
         return OrderModel.find();
     }
 
-    // Get a user by ID from the database.
+    // Get a order by ID from the database.
     getById = (uid) => {
         return OrderModel.findById(uid);
     }
 
-    // Update a user by ID in the database.
+    // Update a order by ID in the database.
     updateById = (uid, data) => {
         return OrderModel.updateOne({ _id: uid }, { $set: data })
     }
 
-    // Delete a user by ID from the database.
+    // Delete a order by ID from the database.
     deleteById = (uid) => {
         return OrderModel.deleteOne({ _id: uid })
     }
--- src/dao/product.mongodb.dao.js
+++ src/dao/product.mongodb.dao.js
@@ -4,27 +4,27 @@ import ProductModel from '../models/product.model'
 
 export default class ProductDao {
 
-    // Create a new user in the database.
+    // Create a new product in the database.
     create = (data) => {
         return ProductModel.create(data)
     }
 
-    // Get all users from the database.
+    // Get all products from the database.
     getall = () => {
         return ProductModel.find();
     }
 
-    // Get a user by ID from the database.
+    // Get a product by ID from the database.
     getById = (uid) => {
         return ProductModel.findById(uid);
     }
 
-    // Update a user by ID in the database.
+    // Update a product by ID in the database.
     updateById = (uid, data) => {
         return ProductModel.updateOne({ _id: uid }, { $set: data })
     }
 
-    // Delete a user by ID from the database.
+    // Delete a product by ID from the database.
     deleteById = (uid) => {
         return ProductModel.deleteOne({ _id: uid })
     }
